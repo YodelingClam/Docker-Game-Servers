99b9a0962452971464c6404d64458cc3adaff3cc
Zack <zack@vincentdesign.ca>
2024-03-12 22:41:13 -0500

    clean up add-route.sh file

--- Minecraft/Game-Server/add-route.sh
+++ Minecraft/Game-Server/add-route.sh
@@ -1,5 +1,3 @@
-
 #!/usr/bin/env bash
 
-curr_routes=$(curl -i -H "Accept: application/json" "mc-router:25560/routes")
 curl --header "Content-Type: application/json"   --request POST   --data "{\"serverAddress\": \"$1\", \"backend\": \"$2:25565\"}"   http://mc-router:25560/routes
